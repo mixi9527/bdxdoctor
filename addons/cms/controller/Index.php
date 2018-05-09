9976f59190bea1330dd8a253a4b775390d257430
xhy_eva <krils@qq.com>
2018-05-10 02:13:17 +0800

    cms

---
+++ addons/cms/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace addons\cms\controller;
+
+use think\Config;
+use think\Db;
+
+class Index extends Base
+{
+
+    public function index()
+    {
+        Config::set('cms.title', __('Home'));
+        Config::set('cms.keywords', '');
+        Config::set('cms.description', '');
+        return $this->view->fetch('/index');
+    }
+
+}
--- application/extra/addons.php
+++ application/extra/addons.php
@@ -16,6 +16,10 @@ return array (
     array (
       0 => 'alisms',
     ),
+    'addon_after_upgrade' => 
+    array (
+      0 => 'cms',
+    ),
     'upload_after' => 
     array (
       0 => 'thumb',
@@ -23,6 +27,13 @@ return array (
   ),
   'route' => 
   array (
+    '/cms$' => 'cms/index/index',
+    '/cms/c/[:diyname]' => 'cms/channel/index',
+    '/cms/t/[:name]' => 'cms/tags/index',
+    '/cms/a/[:diyname]' => 'cms/archives/index',
+    '/cms/p/[:diyname]' => 'cms/page/index',
+    '/cms/s' => 'cms/search/index',
+    '/cms/wxapp' => 'cms/wxapp.index/index',
     '/example$' => 'example/index/index',
     '/example/d/[:name]' => 'example/demo/index',
     '/example/d1/[:name]' => 'example/demo/demo1',
